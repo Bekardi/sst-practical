359bfa780ebd4862b28b3b575091714fda45535e
Bekezhan <abdykarimovbekezhan@gmail.com>
2021-11-22 17:09:12 +0000

    added some code

--- index.js
+++ index.js
@@ -5,4 +5,27 @@ const   http = require('http'), //This module provides the Http server functiona
         fs = require('fs'), // this module allows to work with the file system: read and write files back
         xmlParse = require('xslt-processor').xmlParse, //allows to work with xml
         xsltProcess = require('xslt-processor').xsltProcess, // allows us to utilise XSL Transformation 
-        xml2js = require('xml2js')
+        xml2js = require('xml2js'); // this module does XML<-> JSON conversion
+
+const router = express(),
+      server = http.createServer(router);
+
+router.get('/', function(req, res){
+
+   res.writeHead(200, {'Content-Type' : 'text/html'});
+
+   let xml = fs.readFileSync('PaddysCafe.xml', 'utf8'),
+       xsl = fs.readFileSync('PaddysCafe.xsl', 'utf8');
+
+    let doc = xmlParse(xml),
+        stylesheet = xmlParse(xsl);
+
+    let result = xsltProcessor(doc, stylesheet);
+
+    res.end(result.toString());    
+});
+
+server.listen(process.env.PORT || 3000, process.env.IP || "0.0.0.0", function() {
+    const addr = server.address();
+    console.log("Server listening at", addr.address + ":" + addr.port)
+});
